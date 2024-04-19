86dad24db0851e80559635f9ad811d71a8fa550f
Offic <OfficialHaures@outlook.com>
2024-04-20 00:05:34 +0200

    Added KingdomCommands called /kingdom create

--- src/main/java/nl/officialhaures/snow/SnowPix.java
+++ src/main/java/nl/officialhaures/snow/SnowPix.java
@@ -4,6 +4,7 @@ import me.mattstudios.mf.base.CommandManager;
 import nl.officialhaures.snow.commands.KingdomCommands;
 import nl.officialhaures.snow.events.KingdomChatListener;
 import nl.officialhaures.snow.events.PlayerListeners;
+import nl.officialhaures.snow.manager.Database;
 import nl.officialhaures.snow.manager.UtilManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,10 +13,12 @@ public final class SnowPix extends JavaPlugin {
     private UtilManager utilManager;
     private SnowPix instance;
     CommandManager commandManager;
+    Database database;
 
     @Override
     public void onEnable() {
         instance = this;
+        database = new Database();
         commandManager.register(new KingdomCommands(instance));
         utilManager = new UtilManager();
         getServer().getPluginManager().registerEvents(new PlayerListeners(instance), this);
@@ -30,5 +33,9 @@ public final class SnowPix extends JavaPlugin {
 
     public UtilManager getUtilManager() {
         return utilManager;
+    }
+
+    public Database getDatabase() {
+        return database;
     }
 }
--- src/main/java/nl/officialhaures/snow/commands/KingdomCommands.java
+++ src/main/java/nl/officialhaures/snow/commands/KingdomCommands.java
@@ -6,6 +6,8 @@ import nl.officialhaures.snow.SnowPix;
 import nl.officialhaures.snow.manager.Database;
 import org.bukkit.command.CommandSender;
 
+import java.util.List;
+
 @Command("kingdom")
 @Alias("k")
 
@@ -16,7 +18,6 @@ public class KingdomCommands extends CommandBase {
     public KingdomCommands(SnowPix plugin) {
         this.plugin = plugin;
     }
-    Database db = new Database();
 
     @Default
     public void onDefaultChat(final CommandSender sender) {
@@ -27,11 +28,14 @@ public class KingdomCommands extends CommandBase {
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom list - Get all the kingdom names"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom info <kingdom> - Get information on a specific kingdom"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom join <kingdom> - Join a kingdom"));
+        sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom invite <name> - Invite someone to a kingdom"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom leave - Leave your current kingdom"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom kick - Kick a player out of the kingdom"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom togglepvp - Toggled friendlyfire in the kingdom"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom enemy <kingdom> - Declare war on another kingdom"));
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom ally <kingdom> - Make peace with another kingdom"));
+        sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom disband - Disband the kingdom"));
+        sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6/kingdom setrank <name> <rank> - Gives a kingdommember a new rank"));
 
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&l-------------------------"));
     }
@@ -41,19 +45,77 @@ public class KingdomCommands extends CommandBase {
     public void onKingdomCreate(final CommandSender sender, final String name) {
         if (!sender.hasPermission("kingdom.create")) {
             sender.sendMessage(plugin.getUtilManager().getColor().addColor("&cYou do not have permission to create a kingdom."));
+        return;
         }
+
+        List<String> existingKingdoms = plugin.getDatabase().getKingdoms();
+        if(existingKingdoms.contains(name)){
+            sender.sendMessage(plugin.getUtilManager().getColor().addColor("&cA kingdom with that name already exists."));
+            return;
+        }
+
+        String query = "INSERT INTO `kingdoms`(`Name`) VALUES ('[value-1]')";
+        plugin.getDatabase().executeQuery(query);
+        sender.sendMessage(plugin.getUtilManager().getColor().addColor("&aKingdom &e" + name + "&a has been created."));
+
     }
 
     @SubCommand("list")
-    public void onListCommand(final CommandSender sender){
+    public void onListCommand(final CommandSender sender) {
         sender.sendMessage(plugin.getUtilManager().getColor().addColor("&6Kingdom List:"));
-        for (String kingdom : db.getKingdoms()) {
+        for (String kingdom : plugin.getDatabase().getKingdoms()) {
             sender.sendMessage(plugin.getUtilManager().getColor().addColor("&e- " + kingdom));
         }
     }
 
     @SubCommand("info")
     public void onKingdomInfo(final CommandSender sender, final String kingdom) {
+
+    }
+
+    @SubCommand("join")
+    public void onKingdomJoin(final CommandSender sender, final String kingdom) {
+
+    }
+
+    @SubCommand("invite")
+    public void onInviteKingdom(final CommandSender sender, final String name) {
+
+    }
+
+    @SubCommand("leave")
+    public void onLeaveKingdom(final CommandSender sender) {
+
+    }
+
+    @SubCommand("kick")
+    public void onKickPlayer(final CommandSender sender, final String name) {
+
+    }
+
+    @SubCommand("togglepvp")
+    public void onTogglePvp(final CommandSender sender) {
+
+    }
+
+    @SubCommand("enemy")
+    public void onEnemyKingdom(final CommandSender sender, final String kingdom) {
+
+    }
+
+    @SubCommand("ally")
+    public void onAllyKingdom(final CommandSender sender, final String kingdom) {
+
+    }
+    @SubCommand("disband")
+    @Permission("kingdom.king")
+    public void onDisbandKingdom(final CommandSender sender){
+
+    }
+
+    @SubCommand("setrank")
+    @Permission("kingdom.setrank")
+    public void onSetRank(final CommandSender sender, final String name, final String rank){
 
     }
 }
